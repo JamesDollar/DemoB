bb2a5c6df9f01a7672b3275ae89db5d050eaf7dd
Nicholas Trevino <nicholasltrev@gmail.com>
2020-02-06 08:38:10 -0600

    Added Save System

---
+++ DemoProject/Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveSystem
+{
+    public static void SaveData()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/SaveData.txt";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData();
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadData()
+    {
+        string path = Application.persistentDataPath + "/SaveData.txt";
+
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.Log("Save file not found");
+            return null;
+        }
+    }
+}
