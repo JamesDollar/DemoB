3b985d8590120862bd3a3cf08f5b8e9f62954cc1
Nicholas Trevino <nicholasltrev@gmail.com>
2020-02-06 14:07:17 -0600

    Commit Again Again

---
+++ DemoProject/MainMenu/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MainMenuController : MonoBehaviour
+{
+    public MenuState State { get; private set; } = MenuState.None;
+    public static event Action<MenuState> StateChanged = delegate { };
+
+
+   
+
+    void Start()
+    {
+        ChangeState(1);// call root state (1)
+
+    }
+
+
+    public void ChangeState(int stateIndex)
+    {
+        // convert index to state type 1= root, 2,3,etc
+        State = (MenuState) stateIndex;
+        StateChanged.Invoke(State);
+    }
+    public static event Action<string> MissionChanged = delegate { };
+    public string ActiveMission { get; private set; } = "....";
+
+    public void ChangeMission(string missionName)
+    {
+        ActiveMission = missionName;
+        MissionChanged.Invoke(ActiveMission);
+    }
+
+
+
+
+}
---
+++ DemoProject/MainMenu/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public int UIPanel;
+    public int rations;
+    public int morale;
+    public int gold;
+    public int cargo;
+    public int durability;
+    public int ammo;
+
+    //Add constructor with object keeping track of variables as a parameter.
+}
