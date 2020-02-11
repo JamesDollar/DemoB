7ef6bedcdc6f69f22e77db74a685ae44a1abe8b0
Veer <jangalwa.veer@gmail.com>
2020-02-11 15:59:29 -0600

    Scriptable Object Implementation
    made scripts and objects for encounters

---
+++ USE_THIS/DemoB-master/DemoProject/MainMenu/Assets/Scripts/Encounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Encounter", menuName = "Encounter")]
+public class Encounter : ScriptableObject
+{
+    public string Title;
+    [TextArea]
+    public string Description;
+
+    public string Choice1;
+    public string Choice1Cost;
+
+    public string Choice2;
+    public string Choice2Cost;
+
+    public string Choice3;
+    public string Choice3Cost;
+
+    public string Choice4;
+    public string Choice4Cost;
+
+    public void Print()
+    {
+        Debug.Log(Title);
+        Debug.Log(Description);
+    }
+}
---
+++ USE_THIS/DemoB-master/DemoProject/MainMenu/Assets/Scripts/EncounterDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EncounterDisplay : MonoBehaviour
+{
+    public Encounter _encounter;
+
+    public Text _titleText;
+    public Text _descText;
+
+    public Text _choice1Text;
+    public Text _choice1CostText;
+
+    public Text _choice2Text;
+    public Text _choice2CostText;
+
+    public Text _choice3Text;
+    public Text _choice3CostText;
+
+    public Text _choice4Text;
+    public Text _choice4CostText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _titleText.text = _encounter.Title;
+        _descText.text = _encounter.Description;
+
+        _choice1Text.text = _encounter.Choice1;
+        _choice1CostText.text = _encounter.Choice1Cost;
+
+        _choice2Text.text = _encounter.Choice2;
+        _choice2CostText.text = _encounter.Choice2Cost;
+
+        _choice3Text.text = _encounter.Choice3;
+        _choice3CostText.text = _encounter.Choice3Cost;
+
+        _choice4Text.text = _encounter.Choice4;
+        _choice4CostText.text = _encounter.Choice4Cost;
+}
+
+}
