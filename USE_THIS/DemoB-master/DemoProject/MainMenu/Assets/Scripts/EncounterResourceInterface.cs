f6070d63056ad7f9b5aa51a5229db667551711ea
Veer <jangalwa.veer@gmail.com>
2020-02-25 15:23:36 -0600

    Recap Screen UI and Choice Cost

--- USE_THIS/DemoB-master/DemoProject/MainMenu/Assets/Scripts/EncounterResourceInterface.cs
+++ USE_THIS/DemoB-master/DemoProject/MainMenu/Assets/Scripts/EncounterResourceInterface.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EncounterResourceInterface : MonoBehaviour
 {
     [SerializeField] GameObject _encounterInfo = null;
+    [SerializeField] Text _choice1RecapText;
+    [SerializeField] Text _choice2RecapText;
+
     Encounter _encounter;
     ResourceManager _resourceManager;
+   
 
+    string Choice1Cost;
     string Choice1Type;
     int Choice1Amount;
+    string Choice2Cost;
     string Choice2Type;
     int Choice2Amount;
 
@@ -25,9 +32,11 @@ public class EncounterResourceInterface : MonoBehaviour
         //pulls type and amount from encounter choice 1
         Choice1Type = _encounter.Choice1Type;
         Choice1Amount = _encounter.Choice1Amount;
+        Choice1Cost = _encounter.Choice1Cost;
         //feeds info to ResourceManager and updates resource amounts
         Debug.Log("Interfacing resources(Choice1)...");
         _resourceManager.UpdateResources(Choice1Type, Choice1Amount);
+        _choice1RecapText.text = Choice1Cost;
     }
 
     public void Choice2()
@@ -36,8 +45,10 @@ public class EncounterResourceInterface : MonoBehaviour
         //pulls type and amount from encounter choice 2
         Choice2Type = _encounter.Choice2Type;
         Choice2Amount = _encounter.Choice2Amount;
+        Choice2Cost = _encounter.Choice2Cost;
         //feeds info to ResourceManager and updates resource amounts
         Debug.Log("Interfacing resources(Choice2)...");
         _resourceManager.UpdateResources(Choice2Type, Choice2Amount);
+        _choice2RecapText.text = Choice2Cost;
     }
 }
