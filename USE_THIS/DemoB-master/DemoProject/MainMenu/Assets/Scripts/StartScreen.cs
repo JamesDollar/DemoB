ffee2a5980db5f0eb3b1deb53b65d57b83ecfe40
James <jdd170003@utdallas.edu>
2020-02-27 03:41:57 -0800

    Resource manager integers show, it's logic is still bugged

---
+++ USE_THIS/DemoB-master/DemoProject/MainMenu/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour
+{
+    [SerializeField] GameObject StartScreen1;
+    
+    // Start is called before the first frame update
+   public void CloseIt()
+    {
+        bool IsActive = StartScreen1.activeSelf;
+        StartScreen1.SetActive(!IsActive);
+    }
+}
