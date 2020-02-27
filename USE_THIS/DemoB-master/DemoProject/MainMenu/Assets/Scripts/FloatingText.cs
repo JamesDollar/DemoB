64261a1a2928f75fc11b3184436a3c96827c8d33
Nicholas Trevino <nicholasltrev@gmail.com>
2020-02-27 09:51:53 -0600

    Added Floating Text

---
+++ USE_THIS/DemoB-master/DemoProject/MainMenu/Assets/Scripts/FloatingText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingText : MonoBehaviour
+{
+    [SerializeField] Text floatingText;
+
+    void Awake()
+    {
+        Destroy(gameObject, 2f);
+    }
+
+    private void Update()
+    {
+        Vector3 newPosition = transform.position + new Vector3(0, 0.5f, 0);
+        transform.position = newPosition;
+    }
+
+    public void SetText(string txt)
+    {
+        floatingText.text = txt;
+    }
+}
---
+++ USE_THIS/DemoB-master/DemoProject/MainMenu/Assets/Scripts/FloatingTextController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingTextController : MonoBehaviour
+{
+    [SerializeField] GameObject floatingTextPrefab;
+    Canvas canvas;
+
+    private void Awake()
+    {
+        canvas = GetComponentInParent<Canvas>();
+    }
+
+    public void CreateFloatingText(string text, Transform location)
+    {
+        GameObject floatingTxt = Instantiate(floatingTextPrefab);
+
+        floatingTxt.transform.SetParent(canvas.transform);
+        floatingTxt.transform.position = location.position;
+        floatingTxt.GetComponent<FloatingText>().SetText(text);
+    }
+}
